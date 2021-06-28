c1beafac651ee408e2a539d6e42837723f0e1dc8
Ly Nguyen <ly.nguyen@cmetric.vn>
2021-06-28 16:46:35 +0700

    Update solution to 5kyu's maximum subarray sum kata.

--- 5kyu/maximum_subarray_sum/python_sol.py
+++ 5kyu/maximum_subarray_sum/python_sol.py
@@ -12,15 +12,23 @@ If the list is made up of only negative numbers, return 0 instead.
 Empty list is considered to have zero greatest sum.
 Note that the empty list or array is also a valid sublist/subarray.
 """
-test = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-
 def max_sequence(arr):
+    # Assign current sum as 0
     curr_sum = 0
-    max = 0
+    # Assign most possible maximum sum as 0
+    max_sum = 0
+    # If the list is empty then max_sum == 0, use len(arr)
     if len(arr) == 0:
-        return 0
+        max_sum = 0
     else:
+        # for loop to run through each number in the array
         for i in arr:
-            curr_sum = max(curr_sum + i, i)
-            max = max(curr_sum, max)
-        return max
+            curr_sum += i
+            # if the number is negative, the current sum will be returned to 0
+            # so we can calculate the maximum easily
+            if curr_sum < 0:
+                curr_sum = 0
+            # define the maximum sum through each number    
+            elif max_sum < curr_sum:
+                max_sum = curr_sum
+    return max_sum
